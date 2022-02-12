bd00e89949f0aa010a7747407773710b9e3c99a6
Ali Bajwa <ali_bajwa@epam.com>
2022-02-12 07:55:15 +0000

    second solution

--- solution.go
+++ solution.go
@@ -1,7 +1,11 @@
-package solution
+package main
 
 import "github.com/kyokomi/emoji"
 
 func GetMessage() string {
 	return emoji.Sprint("Hello :world_map:!")
+}
+
+func main() {
+	emoji.Print(GetMessage())
 }
