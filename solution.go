72d1356853b3bd6bd5880c99329884b32da29edc
Ali Bajwa <mojtaba.bajwa@gmail.com>
2022-02-12 01:42:53 +0000

    first commit

--- solution.go
+++ solution.go
@@ -1,5 +1,14 @@
-package solution
+package main
+
+import (
+	"github.com/kyokomi/emoji/v2"
+)
 
 func GetMessage() string {
+	emoji.Println("Hello World :world_map:!")
 	return ""
+}
+
+func main() {
+	GetMessage()
 }
